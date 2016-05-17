0d1c1398f95fcea7758fcc63f855f8f88958fdb4
vs4vijay <vs4vijay@gmail.com>
2016-05-18 01:09:33 +0530

    Rubytrickz class

---
+++ rubytrickz.rb
@@ -0,0 +1,15 @@
+class Rubytrickz
+
+  trap(:INT) {
+    puts "This is Interrupt handler, It will execute on Ctrl+C"
+  }
+
+  at_exit {
+    puts "This is Exit hook, It will run on exit."
+  }
+
+  loop do
+    # Infinite loop
+  end
+
+end
